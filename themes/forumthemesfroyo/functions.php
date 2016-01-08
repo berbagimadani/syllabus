dbafbbd12ab63345a937542265e966134d1d017f
berbagimadani <berbagimadani@gmail.com>
2016-01-08 14:27:13 +0700

    first time

---
+++ themes/forumthemesfroyo/functions.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * Twenty Fifteen functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @link https://codex.wordpress.org/Child_Themes
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * {@link https://codex.wordpress.org/Plugin_API}
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ *
+ * @since Twenty Fifteen 1.0
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 660;
+}
+
+ 
+
+if ( ! function_exists( 'forumthemesfroyo_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ *
+ * @since Twenty Fifteen 1.0
+ */
+function forumthemesfroyo_setup() {
+
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on forumthemesfroyo, use a find and replace
+	 * to change 'forumthemesfroyo' to the name of your theme in all the template files
+	 */
+	//load_theme_textdomain( 'forumthemesfroyo', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * See: https://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 825, 510, true );
+
+	// This theme uses wp_nav_menu() in two locations.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu',      'forumthemesfroyo' ),
+		'social'  => __( 'Social Links Menu', 'forumthemesfroyo' ),
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 *
+	 * See: https://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+	) );
+
+}
+endif; // forumthemesfroyo_setup
+add_action( 'after_setup_theme', 'forumthemesfroyo_setup' );
+
+
+if ( ! function_exists( 'forumthemesfroyo_post_thumbnail' ) ) :
+/**
+ * Display an optional post thumbnail.
+ *
+ * Wraps the post thumbnail in an anchor element on index views, or a div
+ * element when on single views.
+ *
+ * @since Twenty Fifteen 1.0
+ */
+function forumthemesfroyo_post_thumbnail() {
+	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+		return;
+	}
+
+	if ( is_singular() ) :
+	?>
+
+	<div class="post-thumbnail">
+		<?php the_post_thumbnail(); ?>
+	</div><!-- .post-thumbnail -->
+
+	<?php else : ?>
+
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
+		<?php
+			the_post_thumbnail( 'post-thumbnail', array( 'alt' => get_the_title() ) );
+		?>
+	</a>
+
+	<?php endif; // End is_singular()
+}
+endif;
+
+ 
+
+/**
+ * JavaScript Detection.
+ *
+ * Adds a `js` class to the root `<html>` element when JavaScript is detected.
+ *
+ * @since Twenty Fifteen 1.1
+ */
+function forumthemesfroyo_javascript_detection() {
+	echo "<script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>\n";
+}
+add_action( 'wp_head', 'forumthemesfroyo_javascript_detection', 0 );
+
+/**
+ * Enqueue scripts and styles.
+ *
+ * @since Twenty Fifteen 1.0
+ */
+function forumthemesfroyo_scripts() { 
+
+	// Add Genericons, used in the main stylesheet.
+	//wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.2' );
+
+	// Load our main stylesheet.
+	//wp_enqueue_style( 'forumthemesfroyo-style', get_stylesheet_uri() );
+
+	// Load the Internet Explorer specific stylesheet.
+	wp_enqueue_style( 'forumthemesfroyo-bootstrap.min.css', get_template_directory_uri() . '/css/bootstrap.min.css', null, '20141010' );
+ 	wp_enqueue_style( 'forumthemesfroyo-style-custome', get_template_directory_uri() . '/css/style.css', null, '20141010' );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
+	if ( is_singular() && wp_attachment_is_image() ) {
+		wp_enqueue_script( 'forumthemesfroyo-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20141010' );
+	}
+
+	wp_enqueue_script( 'forumthemesfroyo-script', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), '20150330', true );
+	 
+}
+add_action( 'wp_enqueue_scripts', 'forumthemesfroyo_scripts' );
+ 
+
+/**
+ * Add a `screen-reader-text` class to the search form's submit button.
+ *
+ * @since Twenty Fifteen 1.0
+ *
+ * @param string $html Search form HTML.
+ * @return string Modified search form HTML.
+ */
+function forumthemesfroyo_search_form_modify( $html ) {
+	return str_replace( 'class="search-submit"', 'class="search-submit screen-reader-text"', $html );
+}
+add_filter( 'get_search_form', 'forumthemesfroyo_search_form_modify' );
+ 
---
+++ themes/forumthemesfroyo/header.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<!-- TOP -->
+  <div class="container-fluid header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="">
+            <div class="row">
+              <div class="col-md-6">xx</div>
+              <div class="col-md-6 account">
+                <a href="">Register</a>
+                <a href="">Login</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- / TOP -->
+
+  <!-- MENU AND LOGO -->
+  <div class="container">
+    <div class="col-md-12">
+      <div class="col-md-6"><img src="http://www.maketheworldcyou.com/wp-content/uploads/2015/03/logo_new.png"></div>
+      <div class="col-md-6">xx</div>
+    </div>
+  </div>
+  <!-- / MENU AND LOGO  -->
+  </header>
+
+  <section>
+    <div class="container">
+      <div class="col-md-6">
+        HOME
+      </div>
+      <div class="col-md-6">..</div>
+
+      <div class="col-md-12">
+        
+    
+
+     
+
+  
